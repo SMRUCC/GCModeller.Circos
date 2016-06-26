3e2125a4a0388b9876c6d5dd5d3b02e112d86f6b
xieguigang <xie.guigang@gmail.com>
2016-06-26 10:07:39 +0800

    add a data extension api for blastnmappings data

--- Test/mchrTest/Module1.vb
+++ Test/mchrTest/Module1.vb
@@ -1,6 +1,7 @@
 ﻿Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Circos
 Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Circos.Configurations.Nodes.Plots
 Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Circos.Karyotype
+Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Circos.TrackDatas
 Imports LANS.SystemsBiology.AnalysisTools.DataVisualization.Interaction.Circos.TrackDatas.NtProps
 Imports LANS.SystemsBiology.NCBI.Extensions.LocalBLAST.Application
 Imports LANS.SystemsBiology.SequenceModel.FASTA
@@ -30,11 +31,16 @@ Module Module1
         circos.Includes.Add(New Configurations.Ticks(circos))
 
         circos.GetIdeogram.Ideogram.show_label = yes
-        circos.GetIdeogram.Ideogram.Spacing.default = "1u"
+        circos.GetIdeogram.Ideogram.Spacing.default = "0.3u"
         circos.chromosomes_units = "1000000"
+
+        Dim hhhh = Function() maps.Hits(circos.SkeletonKaryotype)
+        Dim inn = hhhh.BeginInvoke(Nothing, Nothing)
 
         circos.AddPlotElement(New Histogram(New GCSkew(genome:=fas, karyotype:=circos.SkeletonKaryotype, SlideWindowSize:=4096, Steps:=2048, Circular:=True)))
         circos.AddPlotElement(New Histogram(New GeneGCContent(genome:=fas, karyotype:=circos.SkeletonKaryotype, winSize:=4096, steps:=2048, getValue:=Function(x) x.GC_AT)))
+        circos.AddPlotElement(New HeatMap(New TrackDatas.data(Of TrackDatas.ValueTrackData)(maps.Identities(circos.SkeletonKaryotype))))
+        circos.AddPlotElement(New Histogram(New TrackDatas.data(Of TrackDatas.ValueTrackData)(hhhh.EndInvoke(inn))))
 
         Call circos.Save("x:\test/")
     End Sub
